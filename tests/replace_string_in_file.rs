f3558d69aad14f1f28df8367617162b97c527996
iocron <info@iocron.com>
2023-08-16 02:50:58 +0200

    Code cleanup (mostly removed comments)

--- tests/replace_string_in_file.rs
+++ tests/replace_string_in_file.rs
@@ -1,25 +1,14 @@
 use std::process::Command;
 use std::fs;
-// use std::time::Duration;
-// use std::thread::sleep;
 use serial_test::*;
 
 fn create_test_files() {
-    // Using delay+#[serial] against file_locks/writes/race-conditions
-    // => (the combination of sleep+serial_test works against those problems)
-    // sleep(Duration::from_millis(100)); // Replaced by Command::new()...wait() in test functions
-
     let files = [
         "./tests/example_files/test1.txt", 
         "./tests/example_files/test2.txt",
         "./tests/example_files/folder1/test3.txt", 
         "./tests/example_files/folder2/folder2inner/test4.txt" 
     ];
-
-    // for file in files {
-    //     // Remove file (if exists), not really needed, because fs::write overwrites by default
-    //     fs::remove_file(file).ok();
-    // }
 
     fs::write(files[0], "UTI\nREPLACE_PENDINGabc\neorwpREPLACE_PENDINGei\nii").expect("File ./tests/test1.txt should have been created.");
     fs::write(files[1], "456").expect("File ./tests/test2.txt should have been created.");
