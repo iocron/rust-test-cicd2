9b63261f6f12762dfd22e730db6eab88960f7437
iocron <info@iocron.com>
2023-08-16 02:42:23 +0200

    Improved test1

--- tests/replace_string_in_file.rs
+++ tests/replace_string_in_file.rs
@@ -23,8 +23,20 @@ fn create_test_files() {
 
     fs::write(files[0], "UTI\nREPLACE_PENDINGabc\neorwpREPLACE_PENDINGei\nii").expect("File ./tests/test1.txt should have been created.");
     fs::write(files[1], "456").expect("File ./tests/test2.txt should have been created.");
-    fs::write(files[2], "Some weird \nText and REPLACE_PENDING\nis not game over.").expect("File ./tests/test4.txt should have been created.");
+    fs::write(files[2], "Some weird \nText and REPLACE_PENDING\nis not game over.").expect("File ./tests/test3.txt should have been created.");
     fs::write(files[3], "Bla\nSome REPLACE_PENDING\nshow me.").expect("File ./tests/test4.txt should have been created.");
+}
+
+fn count_occurrences(text_input: &str, search_string: &str) -> usize {
+    let mut count = 0;
+    let mut start = 0;
+
+    while let Some(pos) = text_input[start..].find(search_string) {
+        count += 1;
+        start += pos + search_string.len();
+    }
+
+    count
 }
 
 #[test]
@@ -38,7 +50,15 @@ fn check_if_replace_string_works_in_single_file() {
         .arg("REPLACE_FINISHED")
         .arg("./tests/example_files/test1.txt")
         .spawn()
-        .expect("cargo run failed to spawn.");
+        .expect("cargo run failed to spawn.")
+        .wait()
+        .expect("failed to wait for command");
+
+    let file_content = fs::read_to_string("./tests/example_files/test1.txt").expect("Expect file test1.txt to read.");
+    let occurrences = count_occurrences(&file_content, "REPLACE_FINISHED");
+
+    // The replaced text should have been applied exactly 2 times in test1.txt 
+    assert_eq!(occurrences, 2);
 }
 
 #[test]
@@ -52,5 +72,7 @@ fn check_if_replace_string_works_in_files_recursive() {
         .arg("REPLACE_FINISHED")
         .arg("./tests/example_files/**/*.txt")
         .spawn()
-        .expect("cargo run failed to spawn.");
+        .expect("cargo run failed to spawn.")
+        .wait()
+        .expect("failed to wait for command");
 }
